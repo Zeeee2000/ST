b9cb6031cbc5e3e278bb6c653369891bdbbfc24d
Zeeee2000 <113877943+Zeeee2000@users.noreply.github.com>
2022-11-23 21:48:11 -0800

    Create testing_Moiz.js

---
+++ testing_Moiz.js
@@ -0,0 +1,84 @@
+module.exports={
+
+/*"Test 1":(browser)=>{
+ 
+
+  
+         browser.url('http://localhost:3000/auth/login');
+         browser.setValue('input[type=email','l191132@lhr.nu.edu.pk');
+         browser.setValue('input[type=password','le6314corolla');
+         browser.click('button[type=submit]');
+         //Question
+         browser.click('#root > div > div > aside > nav > div > div > ul > div > li.exp4uno1.css-1o5xf3v.emyths84 > a > div.css-qz4bcb.ew64ucb1');
+         browser.click('#root > div > div > main > div > div > div.css-lis3pc.e1g7wjgm1 > div > a > div')
+         browser.click('#root > div > div > main > div > div > div > div.css-lis3pc.e1g7wjgm1 > div:nth-child(1) > div > a > div > div.overflow-hidden > h3 > div')
+         //browser.click('#root > div > div > main > div > div > div.css-2mi1c6.eb9z3dt3 > div > div.css-1mjo75b.e17ixpr11 > button.Button.hide.sm-show.e17ixpr110.Button--large.css-1ywy1cz.emiw9oj2 > div > div')
+         //browser.click('#\33 -P-0 > div > div:nth-child(1) > div > div.css-15jit08.e8ui3622 > button.Button.e8ui3621.css-5b2c5c.emiw9oj2 > div > div > span')
+         browser.click('#root > div > div > main > div > div > div.css-mypnpf.eb9z3dt6 > main > div.css-ivjpvm.eb9z3dt1.relative > div > div > div.spread.Visualization.z1 > div > div > div.ReactVirtualized__Grid.TableInteractive-header.scroll-hide-all > div > div:nth-child(4) > div.cellData > div')
+         browser.click('body > span > span > div > div > div:nth-child(1) > div > div:nth-child(2) > svg')
+         browser.pause(10000);
+    },*/
+   /* "Test 2":(browser)=>{
+ 
+
+        browser.url('http://localhost:3000/auth/login');
+        browser.setValue('input[type=email','l191132@lhr.nu.edu.pk');
+        browser.setValue('input[type=password','le6314corolla');
+        browser.click('button[type=submit]');
+        //Question
+        browser.click('#root > div > div > aside > nav > div > div > ul > div > li.exp4uno1.css-1o5xf3v.emyths84 > a > div.css-qz4bcb.ew64ucb1');
+        browser.click('#root > div > div > main > div > div > div.css-lis3pc.e1g7wjgm1 > div > a > div')
+        browser.click('#root > div > div > main > div > div > div > div.css-lis3pc.e1g7wjgm1 > div:nth-child(1) > div > a > div > div.overflow-hidden > h3 > div')
+        browser.click('#root > div > div > main > div > div > div.css-mypnpf.eb9z3dt6 > main > div.css-ivjpvm.eb9z3dt1.relative > div > div > div.spread.Visualization.z1 > div > div > div.ReactVirtualized__Grid.TableInteractive-header.scroll-hide-all > div > div:nth-child(4) > div.cellData > div')
+        browser.click('#root > div > div > main > div > div > div.css-mypnpf.eb9z3dt6 > main > div.css-ivjpvm.eb9z3dt1.relative > div > div > div.spread.Visualization.z1 > div > div > div.ReactVirtualized__Grid.TableInteractive-header.scroll-hide-all > div > div.TableInteractive-cellWrapper.TableInteractive-headerCellData.text-medium.text-brand-hover.cursor-pointer.react-draggable.tether-target.tether-abutted.tether-abutted-left.tether-element-attached-top.tether-element-attached-left.tether-target-attached-bottom.tether-target-attached-left > div.cellData > div')
+        browser.click('body > span > span > div > div > div:nth-child(1) > div > div:nth-child(2)')
+        browser.pause(10000);
+   },*/
+   "Test 3":(browser)=>{
+ 
+    /* Given I have user logged into metabase
+     When I click on Our analytics
+      And I select an dashoard
+     And I click the cross button of a Database
+     And I select remove
+     Then system will remove the DB from the Dashboard
+     And show the result  */
+
+    browser.url('http://localhost:3000/auth/login');
+    browser.setValue('input[type=email','l191132@lhr.nu.edu.pk');
+    browser.setValue('input[type=password','le6314corolla');
+    browser.click('button[type=submit]');
+    //Question
+    browser.click('#root > div > div > aside > nav > div > div > div:nth-child(2) > ul > div:nth-child(1) > div > li > a > div.css-qz4bcb.ew64ucb1');
+    browser.click('#root > div > div > main > div > div > div.css-d7wsyz.e1lvwkfs1 > div.relative > table > tbody > tr:nth-child(3) > td:nth-child(2) > a > h3 > div')
+    browser.click('#root > div > div > main > div > div > div > header > div > div > div.css-prarjz.e1qnfijs1 > div > button.Button.text-brand-hover.cursor-pointer.e1gntnml0.Button--onlyIcon.p1.css-tiv8b1.emiw9oj2 > div > svg > path')
+    browser.click('#root > div > div > main > div > div > div > div > div > div > div > div > div:nth-child(1) > div > div.css-1iuos5l.eumzn1k0 > span > span > a > svg')
+    browser.click('body > div:nth-child(7) > div > div > div > div > div > div.ModalBody.px4 > div > div > button.Button.eyw0xx60.Button--danger.css-z37a8g.emiw9oj2 > div > div')
+    browser.pause(10000);
+},
+
+/*"Test 4":(browser)=>{
+    /* Given I have user logged into metabase
+     When I click on Our analytics
+      And I select a dashoard
+     And I click its name
+     And I select edit its name
+     Then system will edit the Dashboards name in the metabase
+     And show the result  */
+  
+    /*browser.url('http://localhost:3000/auth/login');
+    browser.setValue('input[type=email','l191132@lhr.nu.edu.pk');
+    browser.setValue('input[type=password','le6314corolla');
+    browser.click('button[type=submit]');
+    //Question
+    browser.click('#root > div > div > aside > nav > div > div > div:nth-child(2) > ul > div:nth-child(1) > div > li > a > div.css-qz4bcb.ew64ucb1');
+    browser.click('#root > div > div > main > div > div > div.css-d7wsyz.e1lvwkfs1 > div.relative > table > tbody > tr:nth-child(3) > td:nth-child(2) > a > h3 > div')
+    browser.click('#root > div > div > main > div > div > div > header > div > div > div.css-1iktsqa.e1qnfijs3 > div.css-er2atn.e1qnfijs7 > div > textarea')
+    browser.setValue('input[type=dashboard-name-heading','le6314corolla');
+    browser.pause(10000);
+}*/
+
+}
+
+
+
